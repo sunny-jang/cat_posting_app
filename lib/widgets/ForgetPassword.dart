91c859626e426016f9bb8aa62220bfb457fb0306
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-06-21 00:19:44 -0700

    Create Forgot Password page

---
+++ lib/widgets/ForgetPassword.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ForgetPassword extends StatefulWidget {
+  //Because we will use form
+  @override
+  _ForgetPasswordState createState() => _ForgetPasswordState();
+}
+
+class _ForgetPasswordState extends State<ForgetPassword> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailControllor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Forget password"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: _emailControllor,
+              decoration: InputDecoration(
+                icon: Icon(Icons.account_circle),
+                labelText: "Email",
+              ),
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return "Please input correct Email";
+                }
+                return null;
+              },
+            ),
+            FlatButton(
+              onPressed: () async {
+                await FirebaseAuth.instance
+                    .sendPasswordResetEmail(email: _emailControllor.text);
+
+                final snackBar = SnackBar(
+                  content: Text("Chcck your email for pw reset"),
+                );
+
+                Scaffold.of(context).showSnackBar(snackBar);
+              },
+              child: Text("Reset Password"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
