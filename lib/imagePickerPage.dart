de6edf36cf78d5fe91225a44dd9d4233180b9fec
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-06-24 13:47:42 -0700

    Merge with database code

--- lib/imagePickerPage.dart
+++ lib/imagePickerPage.dart
@@ -1,6 +1,8 @@
-
 import 'dart:io';
 
+import 'package:catpostingapp/timeline.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:image_picker/image_picker.dart';
@@ -13,6 +15,8 @@ class ImagePickerPage extends StatefulWidget {
 class _ImagePickerPageState extends State<ImagePickerPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _descriptionController = TextEditingController();
+  final dbRef = FirebaseDatabase.instance.reference().child("posts/cat_list");
+  var userInfo;
   final _picker = ImagePicker();
 
   File _image;
@@ -24,7 +28,6 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
     setState(() {
       _image = file;
     });
-    print(_image);
   }
 
   void openGallery() async {
@@ -36,10 +39,39 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
     });
   }
 
+  void getUserInfo() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    setState(() {
+      userInfo = user;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: NeumorphicAppBar(),
+      appBar: NeumorphicAppBar(
+        actions: [
+          NeumorphicButton(
+            padding: EdgeInsets.all(12),
+            child: Text("done"),
+            onPressed: () async {
+              await getUserInfo();
+
+              dbRef.push().set({
+                "userId": userInfo.uid,
+                "userEmail": userInfo.email,
+                "image": _image,
+                "des": _descriptionController.text,
+              });
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Timeline(),
+                  ));
+            },
+          ),
+        ],
+      ),
       body: Container(
         child: Column(
           children: <Widget>[
