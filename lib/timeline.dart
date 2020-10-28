071639d22458711009eafeb20cdb4927cca30bc3
Ralph Lee <ralph.lee@live.ca>
2020-10-27 22:52:50 -0700

    Edit card styling

--- lib/timeline.dart
+++ lib/timeline.dart
@@ -28,7 +28,10 @@ class Timeline extends StatelessWidget {
           onPressed: () {},
         ),
         title: Center(
-            child: Image.asset("assets/logo.png", width: 150,),
+          child: Image.asset(
+            "assets/logo.png",
+            width: 150,
+          ),
         ),
         actions: <Widget>[
           NeumorphicButton(
@@ -44,11 +47,7 @@ class Timeline extends StatelessWidget {
       body: Container(
         color: Color(0xffe9eff5),
         child: FutureBuilder(
-          future: dbRef
-              .child("cat_list")
-              .orderByChild("uploadTime")
-              .startAt('1')
-              .once(),
+          future: dbRef.child("cat_list").orderByChild("uploadTime").startAt('1').once(),
           builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
             var lists = [];
             if (snapshot.hasData) {
@@ -66,8 +65,7 @@ class Timeline extends StatelessWidget {
                   margin: EdgeInsets.only(top: 30),
                   style: NeumorphicStyle(
                     shape: NeumorphicShape.convex,
-                    boxShape:
-                        NeumorphicBoxShape.roundRect(BorderRadius.circular(30)),
+                    boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(30)),
                     depth: 10,
                     intensity: 0.7,
                     lightSource: LightSource.bottomLeft,
@@ -82,8 +80,7 @@ class Timeline extends StatelessWidget {
                         Row(
                           children: [
                             CircleAvatar(
-                              backgroundImage:
-                                  AssetImage('images/cat-avatar2.jpg'),
+                              backgroundImage: AssetImage('images/cat-avatar2.jpg'),
                               radius: 30,
                             ),
                             SizedBox(
@@ -91,22 +88,17 @@ class Timeline extends StatelessWidget {
                             ),
                             Text(
                               lists[index]["userEmail"].toString(),
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey[700]),
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[700]),
                             ),
                           ],
                         ),
                         Neumorphic(
-                          margin: EdgeInsets.only(top: 15, bottom: 10),
+                          margin: EdgeInsets.only(top: 20, bottom: 10),
                           style: NeumorphicStyle(
                             shape: NeumorphicShape.convex,
-                            boxShape: NeumorphicBoxShape.roundRect(
-                                BorderRadius.circular(30)),
+                            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(30)),
                           ),
-                          child:
-                              Image.network(lists[index]["image"].toString()),
+                          child: Image.network(lists[index]["image"].toString()),
                         ),
                         Container(
                           padding: EdgeInsets.symmetric(vertical: 15),
@@ -114,8 +106,7 @@ class Timeline extends StatelessWidget {
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                padding: const EdgeInsets.symmetric(horizontal: 8.0),
                                 child: Text(
                                   lists[index]["userEmail"].toString(),
                                   style: TextStyle(
@@ -134,8 +125,7 @@ class Timeline extends StatelessWidget {
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0, vertical: 10),
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
                           child: Text(
                             lists[index]["des"].toString(),
                             style: TextStyle(
@@ -178,8 +168,7 @@ class Timeline extends StatelessWidget {
                     intensity: 1,
                     surfaceIntensity: 0.5,
                     depth: 5,
-                    boxShape:
-                        NeumorphicBoxShape.roundRect(BorderRadius.circular(10)),
+                    boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(10)),
                     color: Colors.white,
                     lightSource: LightSource.topLeft,
                   ),
